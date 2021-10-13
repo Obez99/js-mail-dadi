e30294b3964860be1fe883feb5d3c42538353239
Obez99 <giuliogx@live.it>
2021-10-13 15:07:30 +0200

    Removes isNaN check

--- js/mail.js
+++ js/mail.js
@@ -3,10 +3,7 @@ const emailWhitelist = ["giuliogulizia@gmail.com", "florianleica@gmail.com", "cl
 console.log(emailWhitelist);
 //2 Chiedere all'utente di inserire la propria mail;
 const userEmail = prompt("Inserisci la tua email").toLowerCase();
-  //2.1 Verificare che l'email inserita non sia un numero;
-if (!isNaN(userEmail)){
-  alert("Inserisci un'email valida.");
-}
+
 //3 Controllare che l'email inserita corrisponda ad almeno una delle
 //   email presenti nella whitelist
 let emailCheck = false;
@@ -20,5 +17,5 @@ for(let i=0; i < emailWhitelist.length; i++){
 if (emailCheck === true){
   alert("Complimenti! La tua email è presente nella whitelist.");
 }else{
-  alert("Mi dispiace, la tua email non è presente nella whitelist.");
+  alert("Inserisci un'email valida.");
 }
