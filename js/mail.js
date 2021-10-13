2f1ade44cd9ceec39875829c02a46e20e15cf476
Obez99 <giuliogx@live.it>
2021-10-13 15:05:38 +0200

    Converts user's email to lowercase

--- js/mail.js
+++ js/mail.js
@@ -2,7 +2,7 @@
 const emailWhitelist = ["giuliogulizia@gmail.com", "florianleica@gmail.com", "cleliafradella@gmail.com", "flaviocordari@gmail.com"];
 console.log(emailWhitelist);
 //2 Chiedere all'utente di inserire la propria mail;
-const userEmail = prompt("Inserisci la tua email");
+const userEmail = prompt("Inserisci la tua email").toLowerCase();
   //2.1 Verificare che l'email inserita non sia un numero;
 if (!isNaN(userEmail)){
   alert("Inserisci un'email valida.");
@@ -18,7 +18,7 @@ for(let i=0; i < emailWhitelist.length; i++){
 }
 
 if (emailCheck === true){
-  alert("Complimenti! La tua email è presente nella whitelist.")
+  alert("Complimenti! La tua email è presente nella whitelist.");
 }else{
-  alert("Mi dispiace, la tua email non è presente nella whitelist.")
+  alert("Mi dispiace, la tua email non è presente nella whitelist.");
 }
