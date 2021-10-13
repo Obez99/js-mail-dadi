9809b688f44f82b5bdfbf533985cd8a99f7ad707
Obez99 <giuliogx@live.it>
2021-10-13 15:00:31 +0200

    Checks if user's email is in the whitelist

--- js/mail.js
+++ js/mail.js
@@ -5,7 +5,20 @@ console.log(emailWhitelist);
 const userEmail = prompt("Inserisci la tua email");
   //2.1 Verificare che l'email inserita non sia un numero;
 if (!isNaN(userEmail)){
-  alert("Inserisci un'email valida.")
+  alert("Inserisci un'email valida.");
 }
 //3 Controllare che l'email inserita corrisponda ad almeno una delle
 //   email presenti nella whitelist
+let emailCheck = false;
+
+for(let i=0; i < emailWhitelist.length; i++){
+  if(userEmail === emailWhitelist[i]){
+    emailCheck = true;
+  }
+}
+
+if (emailCheck === true){
+  alert("Complimenti! La tua email è presente nella whitelist.")
+}else{
+  alert("Mi dispiace, la tua email non è presente nella whitelist.")
+}
