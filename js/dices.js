f42e9c6d3fb30fd63c30f277b0dd5040900496dd
Obez99 <giuliogx@live.it>
2021-10-13 15:30:27 +0200

    Assigns a random dice number to player and bot

--- js/dices.js
+++ js/dices.js
@@ -1,4 +1,9 @@
+alert("Benvenuto nel gioco dei dadi. Per iniziare a giocare premi 'OK'");
 //Assegnare un numero casuale da 1 a 6 al giocatore
+const userDice = Math.floor(Math.random()*6 + 1);
+alert(`Il tuo dado è : ${userDice}`);
 //Assegnare un numero casuale da 1 a 6 al computer
+const botDice = Math.floor(Math.random()*6 + 1);
+alert(`Il dado dell'avversario è : ${botDice}`)
 //Confrontare i due numeri
 //Comunicare il vincitore
