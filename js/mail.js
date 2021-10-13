c8e8ba749041fd083e1c9aca763efa48b45c7f45
Obez99 <giuliogx@live.it>
2021-10-13 14:41:30 +0200

    Adds email list in emailWhitelist variable

---
+++ js/mail.js
@@ -0,0 +1,6 @@
+//1 Creare una whitelist di email
+const emailWhitelist = ["giuliogulizia@gmail.com", "florianleica@gmail.com", "cleliafradella@gmail.com", "flaviocordari@gmail.com"];
+console.log(emailWhitelist);
+//2 Chiedere all'utente di inserire la propria mail;
+//3 Controllare che l'email inserita corrisponda ad almeno una delle
+//   email presenti nella whitelist
