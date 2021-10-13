aeae7f96ac767baa88bf3ec3d00c7d53c89e63f8
Obez99 <giuliogx@live.it>
2021-10-13 14:50:06 +0200

    Checks if user input is a number

--- js/mail.js
+++ js/mail.js
@@ -3,5 +3,9 @@ const emailWhitelist = ["giuliogulizia@gmail.com", "florianleica@gmail.com", "cl
 console.log(emailWhitelist);
 //2 Chiedere all'utente di inserire la propria mail;
 const userEmail = prompt("Inserisci la tua email");
+  //2.1 Verificare che l'email inserita non sia un numero;
+if (!isNaN(userEmail)){
+  alert("Inserisci un'email valida.")
+}
 //3 Controllare che l'email inserita corrisponda ad almeno una delle
 //   email presenti nella whitelist
