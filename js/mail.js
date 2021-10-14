0544391e1cad4a57799a1a51814eaf3606da1264
Obez99 <giuliogx@live.it>
2021-10-14 09:08:32 +0200

    Adds visual form in html, edits js script

--- js/mail.js
+++ js/mail.js
@@ -2,20 +2,28 @@
 const emailWhitelist = ["giuliogulizia@gmail.com", "florianleica@gmail.com", "cleliafradella@gmail.com", "flaviocordari@gmail.com"];
 console.log(emailWhitelist);
 //2 Chiedere all'utente di inserire la propria mail;
-const userEmail = prompt("Inserisci la tua email").toLowerCase();
-
+//const userEmail = prompt("Inserisci la tua email").toLowerCase();
+const emailElement = document.getElementById("inputEmail");
 //3 Controllare che l'email inserita corrisponda ad almeno una delle
 //   email presenti nella whitelist
 let emailCheck = false;
 
-for(let i=0; i < emailWhitelist.length; i++){
-  if(userEmail === emailWhitelist[i]){
-    emailCheck = true;
+const formButton = document.getElementById("formButton");
+formButton.addEventListener("click", function () {
+  const userEmail = emailElement.value.toLowerCase();
+  for (let i = 0; i < emailWhitelist.length; i++) {
+    if (userEmail === emailWhitelist[i]) {
+      emailCheck = true;
+    }
   }
-}
+  if (emailCheck === true) {
+    alert("Complimenti! La tua email è presente nella whitelist.");
+    emailCheck = false;
+  } else {
+    alert("Inserisci un'email valida.");
+    emailCheck = false;
+  }
+})
 
-if (emailCheck === true){
-  alert("Complimenti! La tua email è presente nella whitelist.");
-}else{
-  alert("Inserisci un'email valida.");
-}
+
+
